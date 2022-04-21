279841727bd794a87cb613accbe2c1593e7ed989
unknown <tongriva@gmail.com>
2022-04-21 17:38:35 +0800

    docs: little change

---
+++ src/views/city/City.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="city"><city-header></city-header></div>
+</template>
+
+<script>
+import CityHeader from './components/CityHeader.vue'
+export default {
+  name: 'City',
+  components: { CityHeader }
+}
+</script>
+
+<style></style>
